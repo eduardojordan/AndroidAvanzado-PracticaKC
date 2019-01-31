1ff47fc65447471fe7849729083818d84cb596ba
Eduardo Jordan <eduardojordan@hotmail.com>
2019-01-31 18:43:48 +0100

    Remake Todo Again

---
+++ app/src/main/java/io/keepcoding/todo/ui/DetailTask.kt
@@ -0,0 +1,14 @@
+package io.keepcoding.todo.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import io.keepcoding.todo.R
+
+
+class DetailTask : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_task)
+    }
+}
