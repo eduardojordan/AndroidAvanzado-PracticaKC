89a2880248ad30bee3d82f4eb69a0d92e22e1b1d
Eduardo Jordan <eduardojordan@hotmail.com>
2019-01-31 18:54:47 +0100

    DetailTask Task add

--- app/src/main/java/io/keepcoding/todo/ui/DetailTask.kt
+++ app/src/main/java/io/keepcoding/todo/ui/DetailTask.kt
@@ -1,8 +1,10 @@
 package io.keepcoding.todo.ui
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import io.keepcoding.todo.R
+import io.keepcoding.todo.data.model.Task
 
 
 class DetailTask : AppCompatActivity() {
@@ -10,5 +12,15 @@ class DetailTask : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail_task)
+        var task: Task? = null
+
+
+        task = intent.getParcelableExtra("task")
+        if (task == null) {
+            setResult(Activity.RESULT_OK)
+            finish()
+        }
+
+
     }
 }
